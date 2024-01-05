178be7468ea678fc0d68fda33e8e55deee4a266c
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-05 17:44:29 -0600

    add default route to app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
 import Work from './components/Work';
 import Projects from './components/Projects';
 import Interests from './components/Interests';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
+
+        <Route path="/" element={<Navigate replace to="/about" />} />
+
+        
         <Route path="/about" element={<About />} />
         <Route path="/work" element={<Work />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/interests" element={<Interests />} />
-        {/* Add more routes as needed */}
+        
       </Routes>
     </Router>
   );
