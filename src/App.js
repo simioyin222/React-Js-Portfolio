63ca8805e1725e2c356fc2216441556ec537b7bb
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-05 14:29:35 -0600

    app.js udated connection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
 import Work from './components/Work';
@@ -10,13 +10,13 @@ function App() {
   return (
     <Router>
       <Header />
-      <Switch>
-        <Route path="/about" component={About} />
-        <Route path="/work" component={Work} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/interests" component={Interests} />
-        {/* add routes  */}
-      </Switch>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/interests" element={<Interests />} />
+        {/* Add more routes as needed */}
+      </Routes>
     </Router>
   );
 }
