28081853afa97ea5f69975c85ee797fd8f5606d3
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-05 13:48:31 -0600

    add projects setion information/text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,28 @@ function Projects() {
   return (
     <div>
       <h3>Projects</h3>
-      {/* projects here */}
+      <p>
+        I want to mention I only have two projects to show off due to still
+        learning.
+      </p>
+      <ul>
+        <li>
+          This project "PracticeW1" it uses HTML and CSS. This project is
+          practice project that I have been using all week to learn and practice
+          at my first week at Epicodus.
+          <a href="https://github.com/simioyin222/PracticeW1">
+            <span className="work-highlight">click here</span>
+          </a>
+        </li>
+        <li>
+          This project "Simi-Portfolio1" it uses HTML and CSS. This project is
+          what I am currently in real time working on and hopefully submitting
+          for 100%.
+          <a href="https://github.com/simioyin222/SIMI-PORTFOLIO1">
+            <span className="work-highlight">click here</span>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
