1917032281eeb43938e5143465f5d6233c59b78d
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-05 11:16:56 -0600

    add base functionality to projects.js and interests.js

---
+++ src/components/Interests.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Interests() {
+  return (
+    <div>
+      <h4>Interests and Hobbies</h4>
+      {/* interests and hobbies */}
+    </div>
+  );
+}
+
+export default Interests;
---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div>
+      <h3>Projects</h3>
+      {/* projects here */}
+    </div>
+  );
+}
+
+export default Projects;
