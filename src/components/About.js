75470e68aa0393a5722b055613c5b1aa4927e63e
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-05 12:59:51 -0600

    add about me section with text/details

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,15 @@ function About() {
   return (
     <div>
       <h2>About Me</h2>
-      {/* my content */}
+      <p>
+        My name is <em>Similoluwa Oyinkolade</em>, but people usually call me
+        Simi. What's really important to know about me is that I moved to
+        Chicago not too long ago and I'm loving it so far! I'm a full-time
+        streamer and content creator and I enjoy a lot of other things within
+        those ranges as well. Currently, I'm working towards becoming a software
+        engineer/video game developer. My time at Epicodus has been good so far
+        and I'm hopeful for more positive experiences on this long road ahead.
+      </p>
     </div>
   );
 }
