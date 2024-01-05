0e1c2bcdb1aeef0381d7d0a4d1478bd035fc82de
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-05 13:31:20 -0600

    add work experience

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,7 +4,21 @@ function Work() {
   return (
     <div>
       <h3>Work Experience/Background</h3>
-      {/* work experience */}
+      <p>
+        <a href="https://www.linkedin.com/in/simioyin/">LinkedIn for my details</a>
+      </p>
+      <p>
+        In the above link, you can find out more about me. I came to Epicodus in
+        order to further my programming learning and to become a video game
+        developer.
+      </p>
+      <ol>
+        <li>
+          Student Software Engineer-Streamer-Video Games: TobiLLC · Full-time
+        </li>
+        <li>Sales Development Representative: Grubhub · Full-time</li>
+        <li>Store Manager: Gamestop · Full Time</li>
+      </ol>
     </div>
   );
 }
