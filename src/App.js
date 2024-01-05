68cc88696d94ec04db5165c1ed98d34c8f10cd17
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-05 12:16:41 -0600

    change app/js to adjust and connect to other componenet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import About from './components/About';
+import Work from './components/Work';
+import Projects from './components/Projects';
+import Interests from './components/Interests';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/work" component={Work} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/interests" component={Interests} />
+        {/* add routes  */}
+      </Switch>
+    </Router>
   );
 }
 
