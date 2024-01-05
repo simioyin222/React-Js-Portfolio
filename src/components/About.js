af3d6c6055159bd1d9bbedcddf4cc71f708fc3fb
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-05 10:54:50 -0600

    add base code/functionality to header.js, about.js and work.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Me</h2>
+      {/* my content */}
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Work.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Work() {
+  return (
+    <div>
+      <h3>Work Experience/Background</h3>
+      {/* work experience */}
+    </div>
+  );
+}
+
+export default Work;
