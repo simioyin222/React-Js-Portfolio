27408562dc849650600448d01f673d9997c98337
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-05 19:25:26 -0600

    attempting to change styles and color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import styles from './header.module.css';
 
 function Header() {
   return (
     <header>
       <h1>My Portfolio</h1>
-      {/* navigation links or logo here */}
     </header>
   );
 }
