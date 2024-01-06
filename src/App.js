e5901ec8fd8dd13783e498c0f541284b4182e647
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-05 18:00:56 -0600

    removed routes and put normal deafults for one page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
 import Work from './components/Work';
@@ -9,20 +8,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Router>
+    <div>
       <Header />
-      <Routes>
-        
-        <Route path="/" element={<About />} />
-
-        
-        <Route path="/about" element={<About />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/interests" element={<Interests />} />
-        
-      </Routes>
-    </Router>
+      <About />
+      <Work />
+      <Projects />
+      <Interests />
+      
+    </div>
   );
 }
 
