1156fe57f1b7da472235b3ce26f1ff6c96c810f8
simioyin222 <simi.oyinkolade@gmail.com>
2024-03-18 18:40:59 -0500

    set up blog functionality then will implement routing styling

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Blog({ posts }) {
+  return (
+    <div>
+      {posts.map(post => (
+        <article key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.summary}</p>
+        </article>
+      ))}
+    </div>
+  );
+}
+
+export default Blog;
