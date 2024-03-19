9ca2a2859f82dbeb4a73ed0bc77dc2ee300956c6
simioyin222 <simi.oyinkolade@gmail.com>
2024-03-19 14:02:21 -0500

    moved about section on the right side of picture and shrotned skills bars and moved under image now for own seciton as well

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,6 @@ import { useSpring, animated } from 'react-spring';
 function About() {
   const props = useSpring({ opacity: 1, from: { opacity: 0 } });
 
-  // Example skills data
   const skills = [
     { name: 'JavaScript', level: '80%' },
     { name: 'React', level: '70%' },
@@ -15,21 +14,23 @@ function About() {
 
   return (
     <animated.div style={props} className={styles.aboutSection}>
-      <h2>About Me</h2>
       <div className={styles.profilePicContainer}>
-      <img src="../../img/IMG_3456.JPG" alt="Similoluwa Oyinkolade" className={styles.profilePic}/> 
-      </div>
-      <p>My name is <em>Similoluwa Oyinkolade</em>, but people usually call me Simi...</p>
-      <div className={styles.skills}>
-        <h3>Skills</h3>
-        {skills.map((skill, index) => (
-          <div key={index} className={styles.skill}>
-            <div className={styles.skillName}>{skill.name}</div>
-            <div className={styles.skillBar}>
-              <div className={styles.skillLevel} style={{ width: skill.level }}></div>
+        <img src="/img/IMG_3456.JPG" alt="Similoluwa Oyinkolade" className={styles.profilePic}/> 
+        <div className={styles.skills}>
+          <h3>Skills</h3>
+          {skills.map((skill, index) => (
+            <div key={index} className={styles.skill}>
+              <div className={styles.skillName}>{skill.name}</div>
+              <div className={styles.skillBar}>
+                <div className={styles.skillLevel} style={{ width: skill.level }}></div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+      </div>
+      <div className={styles.aboutText}>
+        <h2>About Me</h2>
+        <p>My name is <em>Similoluwa Oyinkolade</em>, but people usually call me Simi...</p>
       </div>
     </animated.div>
   );
