ea155fc0aa8d2d8a3f51e59dd704a7a64dca3e29
simioyin222 <simi.oyinkolade@gmail.com>
2024-03-20 17:03:27 -0500

    made bubbles for intrestes and hobbies fixing how they are displayed but for the most part fuicntionaing and viewing correctly

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './interests.module.css';
 
 function Interests() {
+  const [selectedInterest, setSelectedInterest] = useState(null);
+
+  const interests = [
+    { name: 'MMA/UFC', content: 'Content about MMA/UFC' },
+    { name: 'Video Games', content: 'Content about Video Games' },
+    { name: 'Working Out', content: 'Content about Working Out' },
+    { name: 'Anime', content: 'Content about Anime' },
+    { name: 'Streaming', url: 'https://www.twitch.tv/tobbdawgg' },
+  ];
+
+  const handleClick = (interest) => {
+    setSelectedInterest(interest);
+  };
+
   return (
     <div>
       <h4>Interests and Hobbies</h4>
-      <ul>
-        <li>MMA/UFC</li>
-        <li>Video Games</li>
-        <li>Working Out</li>
-        <li>Anime</li>
-        <li>
-          <a href="https://www.twitch.tv/tobbdawgg">
-            <span className="Streaming"></span> I stream too! Check me out!
-          </a>
-        </li>
-      </ul>
+      <div className={styles.bubblesContainer}>
+        {interests.map((interest, index) => (
+          <div
+            key={index}
+            className={styles.bubble}
+            onClick={() => handleClick(interest)}
+          >
+            {interest.name}
+          </div>
+        ))}
+      </div>
+
+      {selectedInterest && (
+        <div className={styles.detailView}>
+          <h5>{selectedInterest.name}</h5>
+          <p>{selectedInterest.content}</p>
+          {selectedInterest.url && <a href={selectedInterest.url}>Visit</a>}
+        </div>
+      )}
     </div>
   );
 }
